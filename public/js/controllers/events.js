76b7ad5c965ee997177ea160417a0bf4b3664e2e
Alice Kindheart <moonstonecowgirl@gmail.com>
2017-09-25 10:08:10 -0400

    working on sendgrid

--- public/js/controllers/events.js
+++ public/js/controllers/events.js
@@ -733,8 +733,10 @@ angular.module('mean.events').controller('EventController', ['$window', '$filter
     };
 
     $scope.sendgridemail= function(){
+        var sgMail = require('@sendgrid/mail');
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY);
         var msg = {
-          to: 'test@example.com',
+          to: 'moonstonecowgirl@gmail.com',
           from: 'test@example.com',
           subject: 'Sending with SendGrid is Fun',
           text: 'and easy to do anywhere, even with Node.js',
